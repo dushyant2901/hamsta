800d76a7ad648e5647475fb076f11ea17d5aebe7
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-23 11:10:27 +0530

    fix: sidebar z-index

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,13 +3,14 @@ import { CloseIcon } from "../../icons";
 import NavLinks from "../NavLinks";
 
 const Sidebar = ({ setSideBarOpen }) => {
+  // TODO: smooth transition while opening and closing of sidebar.
   return (
-    <div className="w-2/4 absolute top-0 bottom-0 left-0 flex flex-col  md:hidden p-4 bg-slate-50">
+    <aside className="w-2/4 absolute top-0 bottom-0 left-0 flex flex-col  md:hidden p-4 bg-slate-50 z-20">
       <div className="sidebar-top mb-5" onClick={() => setSideBarOpen(false)}>
         <CloseIcon />
       </div>
       <NavLinks type="sidebar" />
-    </div>
+    </aside>
   );
 };
 
