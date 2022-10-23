a8a9f133e9093c9a3a5f360f9005a2afe757a4f1
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-23 13:51:42 +0530

    feat: static singleproduct page

--- src/components/Shared/Button.jsx
+++ src/components/Shared/Button.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ text }) => {
+const Button = ({ text, type }) => {
   return (
     <Link
       to="/products"
-      className="w-fit py-2 px-4 text-lg lg:text-2xl lg:py-3 lg:px-5 inline-block rounded-lg bg-pink text-white hover:text-white hover:bg-black"
+      className={`w-fit py-2 px-4 text-lg lg:text-2xl lg:py-3 lg:px-5 inline-block rounded-lg bg-pink text-white hover:text-white hover:bg-black ${
+        type === "cart-btn" ? "lg:py-2 lg:px-3" : ""
+      }`}
     >
       {text}
     </Link>
