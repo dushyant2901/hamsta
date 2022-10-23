2eee1fbb056631958ec24a9baa185c679702a645
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-23 16:59:05 +0530

    fix: minor ui bugs

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CartFillIcon, WishListIcon } from "../../icons";
 const ProductCard = ({ item }) => {
   return (
-    <article className="xs-[85%] sm:w-[45%] md:w-1/4 lg:w-1/5 xl:w-1/6 flex flex-col  justify-center   rounded-xl p-3 hover:shadow-xl group relative md:mx-5 md:my-5 lg:mx-4 lg:my-4">
+    <article className="w-[80%] xs:w-[45%] sm:w-[45%] md:w-1/4 lg:w-1/5 xl:w-1/6 flex flex-col  justify-center items-center  rounded-xl  hover:shadow-xl group relative mx-2 my-3 md:mx-4 md:my-5 lg:mx-3 lg:my-4  border-[1px] border-pink sm:hover:scale-105 hover:scale-[1.025] transition-all">
       <div className="w-full object-cover  rounded-lg overflow-hidden">
         <img
           src="image/testimage.jpg"
@@ -15,10 +15,10 @@ const ProductCard = ({ item }) => {
         {/* TODO: second image on hover */}
       </div>
 
-      <div className="invisible absolute top-6 left-4 bottom-0   w-fit group-hover:visible  ">
+      <div className="invisible absolute top-2 left-4 bottom-0   w-fit group-hover:visible  ">
         <WishListIcon />
       </div>
-      <div className="py-2 flex flex-col items-center relative">
+      <div className="pb-4 px-3  sm:px-3 flex flex-col items-center relative">
         <div>
           <p className="text-pink text-lg lg:text-xl">Shirt</p>
           <p className="text-md my-1 lg:text-lg font-krona">
@@ -28,7 +28,7 @@ const ProductCard = ({ item }) => {
             <p className="text-xl lg:text-xl mt-1 text-dark-grey font-bold tracking-wider">
               $12.00
             </p>
-            <div className="invisible  w-fit group-hover:visible absolute -bottom-[5%]    right-0 -translate-y-1/4">
+            <div className="invisible  w-fit group-hover:visible absolute bottom-[2.5%]    right-[4%] -translate-y-1/4">
               <CartFillIcon />
             </div>
           </div>
